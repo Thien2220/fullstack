2cfbe757efe1d5d2447f234120a2c83c8ea73178
thien <lengocthien28820@gmail.com>
2023-06-12 14:56:51 +0700

    database-nodejs

---
+++ backend/database.js
@@ -0,0 +1,39 @@
+import mysql from "mysql2";
+import env from "dotenv";
+env.config();
+console.log();
+
+const pool = mysql
+  .createPool({
+    host: process.env.sql_host,
+    user: "newuser",
+    password: "Thien123@",
+    database: "cinema_booking_system",
+  })
+  .promise();
+
+export const getNote = async () => {
+  const [row] = await pool.query("select * from count");
+  return row;
+};
+// const data = await getNote();
+// console.log(data);
+export const changeNote = async (val, id) => {
+  const [result] = await pool.query(
+    "UPDATE  count  SET  countvalue  = ? WHERE (id = ?)",
+    [val, id]
+  );
+  return result;
+};
+// const result = await changeNote(101, 1);
+// console.log(result);
+
+export const createNote = async (countValue) => {
+  const [result] = await pool.query(
+    "INSERT INTO `count` (`countvalue`) VALUES ('?')",
+    [countValue]
+  );
+  return result;
+};
+const resultInsert = await createNote(10);
+console.log(resultInsert);
